f7df7b6cf0c5b2be3152dd138149af2ca7a29198
Vedran <vedran.bre@gmail.com>
2022-01-12 21:10:37 +0100

    margin fix

--- src/components/AbotusItem.vue
+++ src/components/AbotusItem.vue
@@ -1,14 +1,12 @@
 <template>
     <div class="flex justify-center items-center md:max-w-xs flex-col">
-        <img :src=this.props.icon />
-        <h1 class="font-heading font-bold text-white pt-8 text-lg text-center">{{this.props.title}}</h1>
-        <p class="text-center text-purple text-sm pt-3">{{this.props.text}}</p>
+        <img :src=icon />
+        <h1 class="font-heading font-bold text-white pt-8 text-lg text-center">{{title}}</h1>
+        <p class="text-center text-purple text-sm pt-3">{{text}}</p>
     </div>
 </template>
 
 <script lang="ts" setup>
-import { defineProps } from "vue";
-
 const props = defineProps({
     title: String,
     icon: String,
